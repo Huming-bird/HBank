1ae8ad93c85ef8e4595047311ad8fa17edf2d8e6
Huming-bird <ahmedolalekan2@gmail.com>
2023-10-30 15:05:13 -0700

    adding concole codes

--- models/customers.py
+++ models/customers.py
@@ -14,13 +14,12 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-class Customer(Account, Authentication):
+class Customer(BaseModel, Account, Authentication, Base):
     """
     this is the blue print for all customer objects
     """
 
     num_of_cust = 0
-    __acct_num = ""
     first = ""
     last = ""
     dob = ""
@@ -29,7 +28,7 @@ class Customer(Account, Authentication):
     phone = ""
     addr = ""
     mid = ""
-    __passwd = "Huming100&"
+    __passwd = ""
     
     
 
@@ -37,11 +36,5 @@ class Customer(Account, Authentication):
         """
         this method instantiates a customer object
         """
-        #if name_check(first) and name_check(last) and valid_dob(dob) and acct_check(acct_type) and phone_check(phone) and addr_check(addr) and name_check(mid):
         
         super().__init__(*args, **kwargs)
-        self.signup(self.__passwd)    #default password to be changed by customer
-        # self.__acct_num = self.create_acct()
-        # self.__acct_bal = 0
-        # self.__tid_list = []
-        Customer.num_of_cust += 1
